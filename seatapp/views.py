7882555ad864f0b5784c7734c7ffa759721fdc89
AnaMartic <ana.martic2107@gmail.com>
2026-04-24 18:04:37 +0200

    Added login and logout logic

--- seatapp/views.py
+++ seatapp/views.py
@@ -1,8 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
+from django.contrib.auth import logout
 
 # Create your views here.
 def home(request):
     return render(request, 'home.html')
 
 def login_view(request):
-    return render(request, 'login.html')
+    error = None
+
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        else:
+            error = "Unijeli ste neispravnu korisničku oznaku/lozinku"
+
+    return render(request, 'login.html', {'error': error})
+
+def logout_view(request):
+    logout(request)
+    return redirect('home')
