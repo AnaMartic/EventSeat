f198708005f61964e855cde5bba91540cb9b82dc
AnaMartic <ana.martic2107@gmail.com>
2026-04-24 17:08:28 +0200

    Added login page

--- seatapp/admin.py
+++ seatapp/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Event, Table, Guest
 
 # Register your models here.
+admin.site.register(Event)
+admin.site.register(Table)
+admin.site.register(Guest)
--- seatapp/models.py
+++ seatapp/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Event(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    date = models.DateField()
+
+class Table(models.Model):
+    event = models.ForeignKey(Event, on_delete=models.CASCADE)
+    number = models.IntegerField()
+    seats = models.IntegerField()
+    shape = models.CharField(max_length=20)  # okrugao ili četvrtast
+
+class Guest(models.Model):
+    event = models.ForeignKey(Event, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    table = models.ForeignKey(Table, null=True, blank=True, on_delete=models.SET_NULL)    
